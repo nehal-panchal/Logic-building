8e4a9854f8073d6fc659d7ca2873276dafb3149b
Nehal Panchal <nehalpanchal414@gmail.com>
2026-01-11 21:46:01 +0530

    q1

---
+++ q1.java
@@ -0,0 +1,19 @@
+// Take a number and print whether it is positive, negative, or zero. 
+
+import java.util.*;
+
+public class q1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter the number - ");
+        int n = sc.nextInt();
+
+        if(n>0){
+            System.out.println("Positive Number");
+        }else if(n<0){
+            System.out.println("Negative Number");
+        }else{
+            System.out.println("The number is Zero");
+        }
+    }
+}

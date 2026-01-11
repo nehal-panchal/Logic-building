3b6e0213db85e41d1d4aa931aff53ae6bc63785b
Nehal Panchal <nehalpanchal414@gmail.com>
2026-01-11 22:07:24 +0530

    q2

--- q1.java
+++ q1.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class q1{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.err.print("Enter the number - ");
+        System.out.print("Enter the number - ");
         int n = sc.nextInt();
 
         if(n>0){

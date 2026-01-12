639c959fb70c918a6240275bcb99ac2f8ff2c2dc
Nehal Panchal <nehalpanchal414@gmail.com>
2026-01-12 23:16:55 +0530

    q6

---
+++ q6.java
@@ -0,0 +1,8 @@
+// Take two numbers and print the larger one.
+import java.util.*;
+
+public class q6 {
+    public static void main(String[] args) {
+        System.out.println("Enter frist number - ");
+    }
+}

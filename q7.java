36104330bdf990adc155f92d2605358efe41804d
Nehal Panchal <nehalpanchal414@gmail.com>
2026-01-13 22:04:01 +0530

    q7

---
+++ q7.java
@@ -0,0 +1,22 @@
+// Take three numbers and print the largest number.
+import java.util.*;
+
+public class q7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter frist number - ");
+        int first = sc.nextInt();
+        System.out.print("Enter second number - ");
+        int second = sc.nextInt();
+        System.out.print("Enter third number - ");
+        int third = sc.nextInt();
+        
+        if((first > second) &&(first> third) ){
+            System.out.print("First number is greater");
+        }else if(( second > first) &&( second > third ) ){
+            System.out.print("Second number is greater");
+        }else if((third > first)&& (third>second)){
+            System.out.print("Third number is greater");
+        }
+    }
+}

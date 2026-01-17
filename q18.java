0914e429eee0f109d4b183639f9d64ec27fc79c6
Nehal Panchal <nehalpanchal414@gmail.com>
2026-01-17 16:05:14 +0530

    q18

--- q18.java
+++ q18.java
@@ -1,5 +1,16 @@
 // 18. Take an alphabet character and check if it lies between: ‘a’ and ‘m’, or ‘n’ and ‘z’
 
+import java.util.*;
 public class q18 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the character - ");
+        char ch = sc.nextLine().charAt(0);
+        if(ch <= 'm' && ch>= 'a' ){
+            System.out.println("Character lies between 'a' and 'm'");
+        }else{
+            System.out.println("Character lies between 'n' and 'z'");
+        }
+        sc.close();
+    }
 }

fd500f89d033b940d565d392906e092e8e30ad10
Nehal Panchal <nehalpanchal414@gmail.com>
2026-01-17 15:42:46 +0530

    q14

--- q14.java
+++ q14.java
@@ -1,5 +1,26 @@
 // 14. Check if one of two given numbers is a multiple of the other.
+import java.util.*;
 
 public class q14 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number - ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number - ");
+        int num2 = sc.nextInt();
+
+        if (num1 % num2 == 0) {
+            System.out.println("First number is a multiple of second number");
+        }
+        else if (num2 % num1 == 0) {
+            System.out.println("Second number is a multiple of first number");
+        }
+        else {
+            System.out.println("The numbers are not multiples of each other");
+        }
+
+        sc.close();
+    }
 }

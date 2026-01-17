a61b3077a663f79f01da1fb98aaaa6e4cc7f11e0
Nehal Panchal <nehalpanchal414@gmail.com>
2026-01-17 16:09:00 +0530

    q20

--- q20.java
+++ q20.java
@@ -1,5 +1,21 @@
 // 20. Take a month number (1–12) and print the number of days in that month (ignore leap years).
 
+import java.util.*;
 public class q20 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter month number - ");
+        int month = sc.nextInt();
+        if (month == 1 || month == 3 || month == 5 || month == 7 ||
+            month == 8 || month == 10 || month == 12) {
+            System.out.println("31 days");
+        }else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            System.out.println("30 days");
+        }else if (month == 2) {
+            System.out.println("28 days");
+        }else {
+            System.out.println("Invalid month number");
+        }
+        sc.close();
+    }
 }

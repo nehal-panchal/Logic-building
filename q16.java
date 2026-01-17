70c89623ec756308496e8a47b4d2054fdf47c929
Nehal Panchal <nehalpanchal414@gmail.com>
2026-01-17 15:47:06 +0530

    q16

--- q16.java
+++ q16.java
@@ -1,5 +1,17 @@
 // 16. Check voting eligibility for a given age (18+).
-
+import java.util.*;
 public class q16 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter age - ");
+        int age = sc.nextInt();
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        } else if (age >= 0) {
+            System.out.println("Not eligible to vote");
+        } else {
+            System.out.println("Invalid age");
+        }
+        sc.close();
+    }
 }

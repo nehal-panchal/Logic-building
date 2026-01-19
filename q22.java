22fa7ef45626690dd786fb10e10e3787d4145715
Nehal Panchal <nehalpanchal414@gmail.com>
2026-01-19 22:56:42 +0530

    q22

--- q22.java
+++ q22.java
@@ -1,5 +1,24 @@
 // 22. Take a 3-digit number and determine if the middle digit is the largest, smallest, or neither.
 
+import java.util.Scanner;
+
 public class q22 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the 3 digit number - ");
+        int num = sc.nextInt();
+
+        int x = num/100;
+        int y = (num/10)%10;
+        int z = num%10;
+
+        if(y>x && y>z){
+            System.out.println("Middle digit is the largest");
+        }else if(y<x && y<z){
+            System.out.println("Middle digit is the smallest");
+        }else{
+            System.out.println("neither smallest nor largest");
+        }
+        sc.close();
+    }
 }

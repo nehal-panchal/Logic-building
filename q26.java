d418dc91977f4ce64e779b434cd7f72d7433838b
Nehal Panchal <nehalpanchal414@gmail.com>
2026-01-20 23:01:02 +0530

    q26

---
+++ q26.java
@@ -0,0 +1,33 @@
+// 26. Take coordinates (x, y) and determine which quadrant the point lies in.
+
+import java.util.*;
+
+public class q26 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the x coordinate - ");
+        int x = sc.nextInt();
+
+        System.out.print("Enter the y coordinate - ");
+        int y = sc.nextInt();
+
+        if (x > 0 && y > 0) {
+            System.out.println("The point lies in First Quadrant");
+        } else if (x < 0 && y > 0) {
+            System.out.println("The point lies in Second Quadrant");
+        } else if (x < 0 && y < 0) {
+            System.out.println("The point lies in Third Quadrant");
+        } else if (x > 0 && y < 0) {
+            System.out.println("The point lies in Fourth Quadrant");
+        } else if (x == 0 && y == 0) {
+            System.out.println("The point lies at the Origin");
+        } else if (x == 0) {
+            System.out.println("The point lies on Y axis");
+        } else {
+            System.out.println("The point lies on X axis");
+        }
+
+        sc.close();
+    }
+}

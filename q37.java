5d921004db62b9d7729df28d1f1f631558b83501
Nehal Panchal <nehalpanchal414@gmail.com>
2026-01-24 22:01:32 +0530

    q37

---
+++ q37.java
@@ -0,0 +1,25 @@
+// 37. Take a single digit (0–9) and print its word form
+
+import java.util.*;
+
+public class q37 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a digit (0-9): ");
+        int digit = sc.nextInt();
+
+        if (digit == 0) System.out.println("Zero");
+        else if (digit == 1) System.out.println("One");
+        else if (digit == 2) System.out.println("Two");
+        else if (digit == 3) System.out.println("Three");
+        else if (digit == 4) System.out.println("Four");
+        else if (digit == 5) System.out.println("Five");
+        else if (digit == 6) System.out.println("Six");
+        else if (digit == 7) System.out.println("Seven");
+        else if (digit == 8) System.out.println("Eight");
+        else if (digit == 9) System.out.println("Nine");
+        else System.out.println("Invalid input");
+
+        sc.close();
+    }
+}

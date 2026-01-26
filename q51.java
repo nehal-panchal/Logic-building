e7859a2293101acddf1daafec3fc8589ce36b703
Nehal Panchal <nehalpanchal414@gmail.com>
2026-01-26 20:53:18 +0530

    q51

--- q51.java
+++ q51.java
@@ -1,3 +1,9 @@
-public class q51 {
-    
+// 51. Print numbers from 1 to 10.
+
+public class q51{
+    public static void main(String[] args) {
+        for(int i = 1; i<=10;i++){
+            System.out.println(i);
+        }
+    }
 }

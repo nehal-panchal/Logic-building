f25c569402bb54f5ae59d84dd3a426c308046880
Nehal Panchal <nehalpanchal414@gmail.com>
2026-01-27 09:59:26 +0530

    q54

---
+++ q54.java
@@ -0,0 +1,9 @@
+// 54. Print numbers from 10 down to 1. 
+
+public class q54 {
+    public static void main(String[] args) {
+        for(int i = 10; i>=1; i--){
+            System.out.println(i);
+        }
+    }
+}

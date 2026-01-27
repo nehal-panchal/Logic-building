28b0d80df8f3741ebf00373b99100be6c98f2aed
Nehal Panchal <nehalpanchal414@gmail.com>
2026-01-27 08:33:08 +0530

    q53

---
+++ q53.java
@@ -0,0 +1,9 @@
+// 53. Print all odd numbers between 1 and 100.
+
+public class q53 {
+    public static void main(String[] args) {
+        for(int i = 1; i<=100;i+=2){
+            System.out.println(i);
+        }
+    }
+}
